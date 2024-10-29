3c7af5793f623b4b762c1dac40014f558d726dc7
maximillian kodi <mokodi114@gmail.com>
2024-10-29 19:44:58 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,46 @@
+"use client"
+import { api } from "@/convex/_generated/api";
+import { useState } from "react";
+import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
+
 
 export default function Home() {
+
+  //use state to store the messages
+  const messages = useQuery(api.functions.message.list);
+  const createMessage = useMutation(api.functions.message.create);
+
+
+  //const to set the input when user is typing the message
+  const [input, setInput] = useState("");
+
+
+  //function to send the message 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createMessage({ sender: "Alice", content: input });
+    setInput("");
+  }
+
   return (
-    <div>Hello, world</div>
-  );
+    <div>
+
+
+      {
+        //map through the messages
+        messages?.map((message, index) => (
+          <div key={index}>
+            <strong>{message.sender}</strong>: {message.content}
+          </div>
+        ))
+      }
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="message" id="message" value={input} onChange={e => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+
+    </div>
+  )
 }
